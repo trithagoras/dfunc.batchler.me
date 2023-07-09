29fb15bcbd914888b1efd13a91edfd0272dc5209
corey <coreybatchler7@gmail.com>
2023-07-09 12:09:09 +1000

    add editor window

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Editor } from '@monaco-editor/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+      <Editor height="80vh" defaultLanguage="javascript" defaultValue="// some comment" theme='vs-dark' />
+      <button>Compile</button>
+    </div>;
 }
 
 export default App;
